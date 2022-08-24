98f8d529440e7e9632b5331271281620ad30283c
knoriy <knoriy@ip-172-31-36-138.ec2.internal>
2022-08-24 16:40:05 +0000

    modified clap url loader

---
+++ project/utils/get_wds_urls.py
@@ -0,0 +1,49 @@
+import os
+import json
+import random
+
+
+def get_tar_path_from_dataset_name(
+	dataset_names:list[str],
+	dataset_types:list[str],
+	islocal:bool,
+	dataset_path=None,
+	proportion=1,
+):
+	"""
+	Get tar path from dataset name and type
+	"""
+	output = []
+	for n in dataset_names:
+		for s in dataset_types:
+			tmp = []
+			sizefilepath_ = f"./json_files/{n}/{s}/sizes.json"
+			if not os.path.exists(sizefilepath_):
+				continue
+			sizes = json.load(open(sizefilepath_, "r"))
+			for k in sizes.keys():
+				if islocal:
+					tmp.append(f"{dataset_path}/{n}/{s}/{k}")
+				else:
+					tmp.append(
+						f"pipe:aws s3 --cli-connect-timeout 0 cp s3://s-laion-audio/webdataset_tar/{n}/{s}/{k} -"
+					)
+				# TODO: add dataset_path to remote dataset in the future.
+			if proportion!=1:
+				tmp = random.sample(tmp, int(proportion * len(tmp)))
+			output.append(tmp)
+	return sum(output, [])
+
+
+if __name__ == '__main__':
+	urls = get_tar_path_from_dataset_name(
+		['LJSpeech'], 
+		['train', 'test', 'valid'],
+		False, 
+		)
+	print(urls)
+
+	# import boto3 
+	# client = boto3.client('s3')
+	# print(client.list_objects(Bucket='s-laion-audio'))
+
