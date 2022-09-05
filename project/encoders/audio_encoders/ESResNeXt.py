9a827550a8f7c5dbf4d086b72f918a3940158505
knoriy <knoriy@ip-172-31-36-138.ec2.internal>
2022-09-05 10:04:20 +0000

    first commit

---
+++ project/encoders/audio_encoders/ESResNeXt.py
@@ -0,0 +1,4 @@
+'''
+ESResNe(X)t-fbsp: Learning Robust Time-Frequency Transformation of Audio
+https://arxiv.org/abs/2104.11587
+'''
---
+++ project/encoders/audio_encoders/audio_viT.py
@@ -0,0 +1,4 @@
+'''
+AST: Audio Spectrogram Transformer
+https://arxiv.org/abs/2104.01778
+'''
