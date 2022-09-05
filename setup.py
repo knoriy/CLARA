c0ca0b500fe3180c8df059faca885f0d1d1cb646
knoriy <knoriy@ip-172-31-36-138.ec2.internal>
2022-09-05 16:18:13 +0000

    udpated version to 0.0.2

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='clasp',
-    version='0.0.1',
+    version='0.0.2',
     description='Describe Your Cool Project',
     author='Kari Noriy',
     author_email='knoriy72@gmail.com',
