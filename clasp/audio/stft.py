f372c1331e8142112ddce069563abca32040f05c
knoriy <knoriy@gpu-st-p4d-24xlarge-230.hpc-1click-prod450.pcluster>
2022-09-08 12:41:37 +0000

    fixed bug where normlaised audio max or min == 1.0 and -1.0

--- clasp/audio/stft.py
+++ clasp/audio/stft.py
@@ -168,8 +168,8 @@ class TacotronSTFT(torch.nn.Module):
         -------
         mel_output: torch.FloatTensor of shape (B, n_mel_channels, T)
         """
-        assert torch.min(y.data) >= -1
-        assert torch.max(y.data) <= 1
+        assert torch.min(y.data) >= -1.0
+        assert torch.max(y.data) <= 1.0
 
         magnitudes, phases = self.stft_fn.transform(y)
         magnitudes = magnitudes.data
