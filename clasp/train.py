21e375bed6d8d1e72534441f2a6338c51257b30b
knoriy <knoriy@ip-172-31-38-204.ec2.internal>
2022-10-10 16:39:30 +0000

    cleaning code

--- clasp/train.py
+++ clasp/train.py
@@ -1,13 +1,11 @@
 from argparse import ArgumentParser
 
 import torch
-from torch import nn
-from torch.nn import functional as F
-
+import torchmetrics
 import pytorch_lightning as pl
 from pytorch_lightning.callbacks import ModelCheckpoint, EarlyStopping, LearningRateMonitor
 
-from clasp import CLASP
+from clasp import CLASP, SimpleCLASP
 from loss import CLAPLoss
 from datamodules import WebdatasetDataModule, MultilingualWebdatasetDataModule
 from utils.get_wds_urls import get_tar_path_s3
@@ -26,7 +24,7 @@ class PL_CLASP(pl.LightningModule):
 		super().__init__()
 		self.save_hyperparameters()
 
-		self.model = CLASP(self.hparams)
+		self.model = SimpleCLASP(self.hparams)
 		self.loss_fn = CLAPLoss(cache_labels=True)
 
 	def forward(self, batch):
@@ -37,18 +35,21 @@ class PL_CLASP(pl.LightningModule):
 	def training_step(self, batch, batch_idx):
 		text_features, audio_features, tempeture = self(batch)
 		loss = self.loss_fn(text_features, audio_features, tempeture)
+
 		self.log('train_loss', loss, sync_dist=True)
 		return {"loss": loss}
 
 	def validation_step(self, batch, batch_idx):
 		text_features, audio_features, tempeture = self(batch)
 		loss = self.loss_fn(text_features, audio_features, tempeture)
-		self.log('valid_loss', loss, sync_dist=True)
+
+		self.log('valid_loss', loss, sync_dist=True, prog_bar=True)
 
 	def test_step(self, batch, batch_idx):
 		text_features, audio_features, tempeture = self(batch)
 		loss = self.loss_fn(text_features, audio_features, tempeture)
-		self.log('test_loss', loss, sync_dist=True)
+
+		self.log('test_loss', loss, sync_dist=True, prog_bar=True)
 
 	def predict_step(self, batch, batch_idx, dataloader_idx=0):
 		texts, mels = batch
@@ -66,6 +67,8 @@ class PL_CLASP(pl.LightningModule):
 
 	@staticmethod
 	def add_model_specific_args(parent_parser):
+		from text.simple_cleaner.symbols import symbols
+
 		parser = ArgumentParser(parents=[parent_parser], add_help=False)
 		parser.add_argument('--hidden_dim', type=int, default=128)
 		parser.add_argument('--learning_rate', type=float, default=1e-3)
@@ -73,13 +76,13 @@ class PL_CLASP(pl.LightningModule):
 		parser.add_argument('--text_encoder_embedding', type=int, default=1024)
 		parser.add_argument('--text_encoder_layers', type=int, default=1)
 		parser.add_argument('--text_encoder_heads', type=int, default=4)
-		parser.add_argument('--vocab_size', type=int, default=50257)
+		parser.add_argument('--vocab_size', type=int, default=len(symbols))
 
 
 		return parser
 
 def cli_main():
-	pl.seed_everything(1234)
+	pl.seed_everything(9876)
 
 	# ------------
 	# args
@@ -123,6 +126,7 @@ def cli_main():
 		# # 'tmp_eval',
 		# 'BBCSoundEffects', #FAIL
 	]
+	
 	urls = get_tar_path_s3(
 		base_s3_path		= 's-laion-audio/webdataset_tar/', 
 		train_valid_test	= ['train', 'test', 'valid'],
@@ -131,16 +135,20 @@ def cli_main():
 		# recache				= True,
 		)
 
-	for url in urls.values():
-		print(len(url))
+	urls = {
+		'train':['/fsx/knoriy/processed_datasets/clasp_local_data/train/0.tar'], 
+		'test':['/fsx/knoriy/processed_datasets/clasp_local_data/test/0.tar'], 
+		'valid':['/fsx/knoriy/processed_datasets/clasp_local_data/valid/0.tar']
+	}
 
-	dataset = MultilingualWebdatasetDataModule(	
-									train_data_dir = urls['train'][0],
-									test_data_dir = urls['test'][0],
-									valid_data_dir = urls['valid'][0],
-									epochs = args.max_epochs,
-									batch_size = args.batch_size,
-									num_workers = args.num_workers)
+	dataset = WebdatasetDataModule(	
+					train_data_dir = urls['train'],
+					test_data_dir = urls['test'],
+					valid_data_dir = urls['valid'],
+					epochs = args.max_epochs,
+					batch_size = args.batch_size,
+					num_workers = args.num_workers)
+
 	# ------------
 	# model
 	# ------------
@@ -168,16 +176,75 @@ def cli_main():
 	# ------------
 	trainer.test(ckpt_path='best', datamodule=dataset)
 
-	# dataset.setup()
+def test_inference():
+	pl.seed_everything(1234)
 
-	model = model.load_from_checkpoint("/fsx/knoriy/code/CLASP/lightning_logs/version_18646/checkpoints/epoch=29-step=132.ckpt")
+	# ------------
+	# args
+	# ------------
+	parser = ArgumentParser()
+	parser.add_argument('--batch_size', default=64, type=int)
+	parser.add_argument('--num_workers', default=6, type=int)
+
+	parser = pl.Trainer.add_argparse_args(parser)
+	parser = PL_CLASP.add_model_specific_args(parser)
+	args = parser.parse_args()
+
+	# ------------
+	# data
+	# ------------
+	dataset_names = [
+		'EmoV_DB', #PASS
+	]
+	urls = get_tar_path_s3(
+		base_s3_path		= 's-laion-audio/webdataset_tar/', 
+		train_valid_test	= ['train', 'test', 'valid'],
+		dataset_names		= dataset_names, 
+		)
+
+	dataset = WebdatasetDataModule(	
+				train_data_dir = urls['train'][0],
+				test_data_dir = urls['test'][0],
+				valid_data_dir = urls['valid'][0],
+				epochs = args.max_epochs,
+				batch_size = 6,
+				num_workers = args.num_workers)
+	# ------------
+	# model
+	# ------------
+	model = PL_CLASP(args.hidden_dim, args.learning_rate)
+
+	# ------------
+	# training
+	# ------------
+	checkpoint_callback = ModelCheckpoint(monitor="valid_loss")
+	early_stopping_callback = EarlyStopping(monitor="valid_loss")
+	lr_monitor = LearningRateMonitor()
+
+	trainer = pl.Trainer.from_argparse_args(args, 
+		callbacks=[
+			# checkpoint_callback,
+			# early_stopping_callback, 
+			# lr_monitor,
+			],
+	)
+
+	dataset.setup()
+
+	model = model.load_from_checkpoint(
+		"/fsx/knoriy/code/CLASP/lightning_logs/version_1660/checkpoints/epoch=9-step=60.ckpt")
 	texts, mels = next(iter(dataset.test_dataloader()))
 	texts, mels = texts.squeeze(0), mels
 	text_features, audio_features, _ = model((texts, mels))
-	torch.save(text_features, 'a_text_features.pt')
-	torch.save(audio_features, 'a_audio_features.pt')
-	# print(trainer.predict(model, dataloaders=dataset))
 
+	text_features, audio_features = text_features.cpu().detach(), audio_features.cpu().detach()
+
+	audio_features /= audio_features.norm(dim=-1, keepdim=True)
+	text_features /= text_features.norm(dim=-1, keepdim=True)
+	text_probs = (audio_features @ text_features.T).softmax(dim=-1)
+	import matplotlib.pyplot as plt
+	plt.imsave('test.png', text_probs)
 
 if __name__ == '__main__':
 	cli_main()
+	# test_inference()
