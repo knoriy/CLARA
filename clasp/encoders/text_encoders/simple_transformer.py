c6030be3c5066671e8e7d8ed81660e8f4b9fa922
knoriy <knoriy@ip-172-31-38-204.ec2.internal>
2022-10-11 09:36:10 +0000

    move to pytorch implemenatation of transformer

--- clasp/encoders/text_encoders/simple_transformer.py
+++ clasp/encoders/text_encoders/simple_transformer.py
@@ -5,9 +5,9 @@ from typing import Tuple, Union, Callable, Optional
 
 
 class TransformerEncoder(nn.Module):
-    def __init__(self, in_channels: int, out_channels:int, num_layers: int, nhead: int):
+    def __init__(self, in_channels: int, out_channels:int, num_layers: int, nhead: int, batch_first:bool=False):
         super().__init__()
-        encoder_layer = nn.TransformerEncoderLayer(d_model=in_channels, nhead=nhead)
+        encoder_layer = nn.TransformerEncoderLayer(d_model=in_channels, nhead=nhead, batch_first=batch_first)
         self.transformer_encoder = nn.TransformerEncoder(encoder_layer, num_layers=num_layers)
         self.project = nn.Linear(in_channels, out_channels)
 
