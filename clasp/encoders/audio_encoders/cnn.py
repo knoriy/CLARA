c1ced60d30f9d02e752ec6b06e582a4018b2b93a
knoriy <knoriy@gpu-st-p4d-24xlarge-44.hpc-1click-prod450.pcluster>
2022-10-26 17:09:38 +0000

    Update: Updated CovBlock for better training

--- clasp/encoders/audio_encoders/cnn.py
+++ clasp/encoders/audio_encoders/cnn.py
@@ -2,28 +2,20 @@ import torch.nn as nn
 import torch.nn.functional as F
 
 class ConvBlock(nn.Module):
-    '''
-    Simple Conv1d model with batchnorm and dropout
-    '''
     def __init__(self, in_channel:int, out_channel:int) -> None:
         super().__init__()
-        self.conv1d1 = nn.Conv1d(in_channel,out_channel, 3)
-        self.bn1 = nn.BatchNorm1d(out_channel)
-        self.conv1d2 = nn.Conv1d(out_channel,out_channel, 3)
-        self.bn2 = nn.BatchNorm1d(out_channel)
+        self.conv1 = nn.Conv1d(in_channel, 1024, 1)
+        self.conv2 = nn.Conv1d(1024, out_channel, 3)
+        self.bn = nn.BatchNorm1d(out_channel)
 
     def forward(self, x):
-        x = F.relu(self.bn1(self.conv1d1(x)))
-        x = F.dropout(x)
-
-        x = F.relu(self.bn2(self.conv1d2(x)))
-        x = F.dropout(x)
-        return x
+        x = F.relu(self.conv1(x))
+        return F.relu(self.bn(self.conv2(x)))
 
 class Cnn10(nn.Module):
     def __init__(self, n_mels:int, out_channel:int):
         super().__init__()
-        in_sizes = [n_mels] + [128, 256, 512, 1024] + [out_channel]
+        in_sizes = [n_mels] + [128, 246, 512, 1024, 2048] + [out_channel]
         self.layers = nn.ModuleList(
             [ ConvBlock(in_size, out_size)
             for (in_size, out_size) in zip(in_sizes, in_sizes[1:])]
@@ -37,14 +29,14 @@ class Cnn10(nn.Module):
 class Cnn12(nn.Module):
     def __init__(self, n_mels:int, out_channel:int):
         super().__init__()
-        self.block_1 = ConvBlock(n_mels, 128)
-        self.layers = nn.Sequential(
-            ConvBlock(128, 512),
-            ConvBlock(512, 1024),
-            ConvBlock(1024, 1024),
+        in_sizes = [n_mels] + [128, 246, 512, 1024, 2048] + [out_channel]
+        self.layers = nn.ModuleList(
+            [ ConvBlock(in_size, out_size)
+            for (in_size, out_size) in zip(in_sizes, in_sizes[1:])]
         )
+    
     def forward(self, x):
-        x = self.block_1(x)
-        x = self.layers(x)
+        for linear in self.layers:
+            x = linear(x)
         return x
 
