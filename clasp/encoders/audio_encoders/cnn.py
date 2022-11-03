e6c8a63f39a980dac89a0737479d7d94291bbd0b
knoriy <knoriy@ip-172-31-38-204.ec2.internal>
2022-11-03 10:50:44 +0000

    mod: renamed var

--- clasp/encoders/audio_encoders/cnn.py
+++ clasp/encoders/audio_encoders/cnn.py
@@ -5,8 +5,8 @@ import torch.nn.functional as F
 class ConvBlock(nn.Module):
     def __init__(self, in_channel:int, out_channel:int) -> None:
         super().__init__()
-        self.conv1 = nn.Conv1d(in_channel, 1024, 1)
-        self.conv2 = nn.Conv1d(1024, out_channel, 3)
+        self.conv1 = nn.Conv1d(in_channel, out_channel, 1)
+        self.conv2 = nn.Conv1d(out_channel, out_channel, 3)
         self.bn = nn.BatchNorm1d(out_channel)
 
     def forward(self, x):
@@ -16,17 +16,15 @@ class ConvBlock(nn.Module):
 class Cnn10(nn.Module):
     def __init__(self, n_mels:int, out_channels:int):
         super().__init__()
-        # in_sizes = [n_mels] + [1024, 1024, 1024, 1024] + [out_channel]
-        in_sizes = [n_mels] + [1024] +  [out_channels]
+        in_sizes = [n_mels] + [128, 256, 512, 1024, 1024] + [out_channels]
         self.layers = nn.ModuleList(
             [ ConvBlock(in_size, out_size)
             for (in_size, out_size) in zip(in_sizes, in_sizes[1:])]
         )
     
     def forward(self, x):
-        for linear in self.layers:
-            x = linear(x)
-
+        for layer in self.layers:
+            x = layer(x)
         return x
 
 class Cnn12(nn.Module):
