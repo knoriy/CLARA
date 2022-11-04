801dd80cce6e217d173a888de7a857a8e2005b94
knoriy <knoriy@gpu-st-p4d-24xlarge-74.hpc-1click-prod450.pcluster>
2022-11-04 11:24:02 +0000

    add: added conv2d cnn models

--- clasp/encoders/audio_encoders/__init__.py
+++ clasp/encoders/audio_encoders/__init__.py
@@ -1,3 +1,3 @@
 from .whisper import WhisperAudioEncoder
 from .simple_cnn import SimpleCNN, SimpleCNNLarge
-from .cnn import Cnn12, Cnn10
+from .cnn import Cnn1D12, Cnn1D10, Cnn2D10
--- clasp/encoders/audio_encoders/cnn.py
+++ clasp/encoders/audio_encoders/cnn.py
@@ -1,8 +1,9 @@
+import torch
 import torch.nn as nn
 import torch.nn.functional as F
 
 # CNN Networks
-class ConvBlock3x3(nn.Module):
+class ConvBlock1D3x3(nn.Module):
     def __init__(self, in_channel:int, out_channel:int) -> None:
         super().__init__()
         self.conv1 = nn.Conv1d(in_channel, out_channel, 3)
@@ -13,12 +14,12 @@ class ConvBlock3x3(nn.Module):
         x = F.relu(self.conv1(x))
         return F.relu(self.bn(self.conv2(x)))
 
-class Cnn10(nn.Module):
+class Cnn1D10(nn.Module):
     def __init__(self, n_mels:int, out_channels:int):
         super().__init__()
         in_sizes = [n_mels] + [2048, 2048, 2048, 2048] + [out_channels]
         self.layers = nn.ModuleList(
-            [ ConvBlock3x3(in_size, out_size)
+            [ ConvBlock1D3x3(in_size, out_size)
             for (in_size, out_size) in zip(in_sizes, in_sizes[1:])]
         )
     
@@ -27,16 +28,44 @@ class Cnn10(nn.Module):
             x = layer(x)
         return x
 
-class Cnn12(nn.Module):
+class Cnn1D12(nn.Module):
     def __init__(self, n_mels:int, out_channels:int):
         super().__init__()
         in_sizes = [n_mels] + [2048, 2048, 2048, 2048, 2048] + [out_channels]
         self.layers = nn.ModuleList(
-            [ ConvBlock3x3(in_size, out_size)
+            [ ConvBlock1D3x3(in_size, out_size)
             for (in_size, out_size) in zip(in_sizes, in_sizes[1:])]
         )
     
     def forward(self, x):
         for linear in self.layers:
             x = linear(x)
+        return x
+
+class ConvBlock2D3x3(nn.Module):
+    def __init__(self, in_channel:int, out_channel:int) -> None:
+        super().__init__()
+        self.conv1 = nn.Conv2d(in_channel, out_channel, 3)
+        self.conv2 = nn.Conv2d(out_channel, out_channel, 3)
+        self.bn = nn.BatchNorm2d(out_channel)
+
+    def forward(self, x):
+        x = F.relu(self.conv1(x))
+        return F.relu(self.bn(self.conv2(x)))
+
+class Cnn2D10(nn.Module):
+    def __init__(self, in_channels:int, out_channels:int):
+        super().__init__()
+        in_sizes = [in_channels] + [2048, 2048, 2048, 2048] + [out_channels]
+        self.layers = nn.ModuleList(
+            [ ConvBlock2D3x3(in_size, out_size)
+            for (in_size, out_size) in zip(in_sizes, in_sizes[1:])]
+        )
+    
+    def forward(self, x):
+        for layer in self.layers:
+            x = layer(x)
+
+        x = torch.mean(x, dim=3)
+        
         return x
