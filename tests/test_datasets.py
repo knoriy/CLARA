e7f9c840c883b1581a49fdc31916c9cb54a1fb9b
knoriy <knoriy@ip-172-31-44-155.ec2.internal>
2023-01-10 19:22:13 +0000

    add: added test for long string in data

--- tests/test_datasets.py
+++ tests/test_datasets.py
@@ -18,35 +18,44 @@ cleaner = EnglishTextNormalizer()
 tokenizer = Tokeniser()
 def collate_fn(data):
 		raw_audios, raw_texts, __url = data
+
+		# mels = [stft_fn(audio[0][0]).T for audio in raw_audios]
+		# texts = [torch.tensor(
+		# 	tokenizer.encode(
+		# 		cleaner(', '.join(text['text']) if isinstance(text['text'], list) else text['text']), 
+		# 		language = 'en' if 'original_data' not in text.keys() else text["original_data"]["language"] if "language" in text["original_data"].keys() else 'en'
+		# 		)) for text in raw_texts]
+
 		for audio, text , url in zip(raw_audios, raw_texts, __url):
-			# print(audio[0].shape)
+			# Audio
 			if audio[0].shape[1] == 0:
 				print(f"File with 0 length; {audio[0].shape}, URL: {url}")
-			if audio[0].shape[1] > 48000*30:
+			if audio[0].shape[1] > 48000*20:
 				print(f"Big file found; {audio[0].shape[1]} = {audio[0].shape[1]/48000}, URL: {url}")
 			if audio[0].shape[1] < 48000*1:
 				print(f"File found with less than 1 sec; {audio[0].shape[1]} = {audio[0].shape[1]/48000}, URL: {url}")
+			
+			# Text
+			text_len = len(', '.join(text['text']) if isinstance(text['text'], list) else text['text'])
+			if text_len > 1024:
+				print(f"Large string sound: length is {text_len}, URL: {url}")
+			if text_len == 0:
+				print(f"Empty string found: URL: {url}")
 
-		mels = [stft_fn(audio[0][0]).T for audio in raw_audios]
-		texts = [torch.tensor(
-			tokenizer.encode(
-				cleaner(', '.join(text['text']) if isinstance(text['text'], list) else text['text']), 
-				language = 'en' if 'original_data' not in text.keys() else text["original_data"]["language"] if "language" in text["original_data"].keys() else 'en'
-				)) for text in raw_texts]
+		# mel_lengths = [mel.shape[0] for mel in mels]
+		# mel_lengths = torch.tensor(mel_lengths)
+		# text_lengths = [text.shape[0] for text in texts]
+		# text_lengths = torch.tensor(text_lengths)
 
-		mel_lengths = [mel.shape[0] for mel in mels]
-		mel_lengths = torch.tensor(mel_lengths)
-		text_lengths = [text.shape[0] for text in texts]
-		text_lengths = torch.tensor(text_lengths)
+		# texts = pad_sequence(texts).T
+		# mels = pad_sequence(mels).permute(1,2,0)
 
-		texts = pad_sequence(texts).T
-		mels = pad_sequence(mels).permute(1,2,0)
-
-		return texts, mels, text_lengths, mel_lengths
+		# return texts, mels, text_lengths, mel_lengths
 		return torch.rand((1,1))
 
 def test_datasets():
-	data_dir = "pipe:aws s3 --cli-connect-timeout 0 cp s3://s-laion-audio/webdataset_tar/common_voice/train/{1680..1688}.tar -"
+	# data_dir = "pipe:aws s3 --cli-connect-timeout 0 cp s3://s-laion-audio/webdataset_tar/common_voice/train/{0..1695}.tar -"
+	data_dir = "pipe:aws s3 --cli-connect-timeout 0 cp s3://s-laion-audio/webdataset_tar/common_voice/train/227.tar -"
 	batch_size = 64
 
 	pipeline = []
