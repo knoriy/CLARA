640ac9cad78950e650ceccaaecac14d2b0aa8507
knoriy <knoriy@ip-172-31-44-155.ec2.internal>
2023-03-31 15:47:29 +0000

    add: added function to return data files from local disk

--- clasp/utils/get_wds_urls.py
+++ clasp/utils/get_wds_urls.py
@@ -41,7 +41,7 @@ def get_tar_path_from_dataset_name(
 	return sum(output, [])
 
 
-def get_s3_paths(base_s3_path:str, 
+def get_s3_paths(base_path:str, 
 		train_valid_test:list[str], 
 		dataset_names:list[str] or str=[''], 
 		exclude:list[str]=[], 
@@ -59,18 +59,42 @@ def get_s3_paths(base_s3_path:str,
 	# create cmd for collecting url spesific dataset, 
 	# if `dataset_names` is not given it will search the full base_s3_path
 	dataset_names = [dataset_names] if isinstance(dataset_names, str) else dataset_names
-	cmds = [f'aws s3 ls s3://{os.path.join(base_s3_path, name, "")} --recursive | grep /.*.tar' for name in dataset_names]
+	cmds = [f'aws s3 ls s3://{os.path.join(base_path, name, "")} --recursive | grep /.*.tar' for name in dataset_names]
 	# urls are collected
 	urls = [os.popen(cmd).read() for cmd in cmds]
 	# cleaning the urls to conform with webdataset
 	final_urls = [i.split(' ')[-1] for url in urls for i in url.split('\n')]
-	final_urls = [f's3://{os.path.join(base_s3_path, *i.split("/")[1:])}' for i in final_urls]
+	final_urls = [f's3://{os.path.join(base_path, *i.split("/")[1:])}' for i in final_urls]
 	# Spliting url by state e.g. train, test and valud
 	final_urls = {state:[url for url in final_urls if state in url 
 		and all(exclude_name not in url for exclude_name in exclude)] for state in train_valid_test}
 
 	if cache_path:
 		pl_logger.info(f"Creating URL cache: {cache_path}")
+		with open(cache_path, 'w') as f:
+			json.dump(final_urls, f)
+
+	return final_urls
+
+def get_local_paths(base_path:str, 
+		train_valid_test:list[str], 
+		dataset_names:list[str] or str=[''], 
+		exclude:list[str]=[], 
+		cache_path:str='', 
+		use_cache:bool=False, 
+		recache:bool=False,
+	):
+	if os.path.isfile(cache_path) and not recache and use_cache:
+		with open(cache_path) as f:
+			return json.load(f)
+
+	base_paths = [pathlib.Path(base_path)/dataset_name for dataset_name in dataset_names]
+	final_urls = [str(path) for base_path in base_paths for path in base_path.rglob("*.tar")]
+
+	# Spliting url by state e.g. train, test and valud
+	final_urls = {state:[url for url in final_urls if state in url and all(exclude_name not in url for exclude_name in exclude)] for state in train_valid_test}
+
+	if cache_path:
 		with open(cache_path, 'w') as f:
 			json.dump(final_urls, f)
 
