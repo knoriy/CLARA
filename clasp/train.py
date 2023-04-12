b74cdf2356264524f8f3a48413fd55d780e2e25a
knoriy <knoriy@ip-26-0-132-37.ec2.internal>
2023-04-12 15:58:11 +0000

    updated zeroshot

--- clasp/train.py
+++ clasp/train.py
@@ -1,11 +1,14 @@
 
 import logging
-log = logging.getLogger(__name__)
+pl_logger = logging.getLogger('pytorch_lightning')
 
 import torch
 import pytorch_lightning as pl
 from pytorch_lightning.core.datamodule import LightningDataModule
 from pytorch_lightning.cli import LightningCLI
+from pytorch_lightning.utilities.exceptions import MisconfigurationException
+from pytorch_lightning.trainer import call
+
 
 from typing import Dict, Set, Optional
 
@@ -15,20 +18,43 @@ from eval.zeroshot import zeroshot_eval
 from utils import get_lists
 
 class Trainer(pl.Trainer):
+	def __init__(self, zeroshot_templates:str=None, zeroshot_classes:str=None, *args, **kwargs):
+		super().__init__(*args, **kwargs)
+		self.zeroshot_templates = zeroshot_templates
+		self.zeroshot_classes = zeroshot_classes
+
 	def zeroshot(self, 
 		model, 
-		dataloaders, 
-		zeroshot_templates, 
+		dataloaders:Optional[LightningDataModule] = None, 
 		datamodule: Optional[LightningDataModule] = None,
 		ckpt_path: Optional[str] = None,
 	) -> tuple[float, float]:
-		Trainer._log_api_event("zeroshot")
-		log.detail(f"{self.__class__.__name__}: trainer validate stage")
-		templates = get_lists(zeroshot_templates)
+		"""Zeroshot evaluation.""" 
+		pl_logger.debug(f"{self.__class__.__name__}: trainer zeroshot stage")
 
-		classes = ["hello world", "how are you?", "some random thing", "it's a beautiful day", "i love you", "goodbye", "i hate you", "today is not the day"]
+		# if a datamodule comes in as the second arg, then fix it for the user
+		if isinstance(dataloaders, LightningDataModule):
+			datamodule = dataloaders
+			dataloaders = None
+		if dataloaders is not None and datamodule:
+			raise MisconfigurationException("You cannot pass both `trainer.zeroshot(dataloaders=..., datamodule=...)`")
+
+		# links data to the trainer
+		datamodule.setup()
+		self._data_connector.attach_data(model, datamodule=datamodule)
+
+		if not self.zeroshot_templates:
+			raise ValueError("trainer.zeroshot_templates is required, must be a path to a file containing a list of templates (one per line).")
+		templates = get_lists(self.zeroshot_templates)
+
+		if not self.zeroshot_classes:
+			raise ValueError("trainer.zeroshot_classes is required, must be a path to a file containing a list of templates (one per line).")
+		classes = get_lists(self.zeroshot_classes)
+
+		pl_logger.info(f"Zeroshot evaluation with {len(classes)} classes and {len(templates)} templates")
 		
-		return zeroshot_eval(model, classes, templates, dataloaders)
+		acc1, acc5 = zeroshot_eval(model, classes, templates, datamodule.test_dataloader())
+		pl_logger.info(f"zeroshot accuracy@1: {acc1:.3f}, accuracy@5: {acc5:.3f}")
 
 class MyLightningCLI(LightningCLI):
 
