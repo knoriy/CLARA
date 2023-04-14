7a5520dbdd46a6ba1b71aaae05604294489c3923
knoriy <knoriy@ip-172-31-38-94.ec2.internal>
2023-04-14 13:43:57 +0000

    updated collate fn

--- clasp/datamodule/td_tensored.py
+++ clasp/datamodule/td_tensored.py
@@ -24,13 +24,32 @@ class TensoredTDM(MultilingualTDM):
 			.batch(2) \
 			.shuffle(buffer_size=self.batch_size)\
 			.map(self.to_sampels) \
-			# .batch(self.batch_size) \
-			# .map(self.collate_fn)
+			.batch(self.batch_size) \
+			.map(self.collate_fn)
 		
 		return datapipe
 	
 	def collate_fn(self, data):
-		return super().collate_fn(data)
+		def to_token(t):
+			return torch.tensor(
+					self.tokeniser_encode(
+						', '.join(t['text']) if isinstance(t['text'], list) else t['text'], \
+							'en' if 'original_data' not in t.keys() else t["original_data"]["language"] if "language" in t["original_data"].keys() else 'en'
+					)
+				)
+	
+		mels = [a.T for (a, _) in data]
+		texts = [to_token(t) for (_, t) in data]
+
+		mel_lengths = [mel.shape[0] for mel in mels]
+		mel_lengths = torch.tensor(mel_lengths)
+		text_lengths = [text.shape[0] for text in texts]
+		text_lengths = torch.tensor(text_lengths)
+
+		texts = pad_sequence(texts).T.contiguous()
+		mels = pad_sequence(mels).permute(1,2,0).contiguous()
+
+		return texts, mels, text_lengths, mel_lengths
 
 
 if __name__ == '__main__':
