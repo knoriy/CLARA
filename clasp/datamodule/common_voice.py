27e3129bb06b410b126a8a9df1da6717aed7e3d7
knoriy <knoriy@ip-172-31-44-155.ec2.internal>
2023-05-02 13:46:45 +0000

    added common voice tdm

---
+++ clasp/datamodule/common_voice.py
@@ -0,0 +1,86 @@
+import io
+import json
+from typing import Optional
+import soundfile
+import librosa
+import numpy as np
+
+import torch
+import torchdata
+from torch.nn.utils.rnn import pad_sequence
+
+# from .base_tdm import BaseTDM
+from datamodule.base_tdm import BaseTDM, group_by_filename
+from text.tokeniser import Tokeniser # from text.whisper.tokenizer import get_tokenizer
+
+_AGE_DICT = {"teens":0, "twenties": 1, "thirties": 2, "fourties": 3, "fifties": 4, "sixties": 5, "seventies": 6, "eighties": 7, "nineties": 8, "hundreds": 9}
+_GENDER_DICT = {"male": 0,"female": 1}
+
+class CommonVoiceTDM(BaseTDM):
+	def __init__(self, *args, **kwargs):
+		super().__init__(*args, **kwargs)
+		self.tokeniser = Tokeniser()
+
+	def to_sampels(self, data):
+		a, t = data
+		audio, meta = soundfile.read(io.BytesIO(a[1].read())), json.loads(t[1].read().decode('utf-8'))
+
+		labels = {
+				'text':meta["text"],
+				'age':meta["original_data"]["age"],
+				'gender':meta["original_data"]["gender"],
+				'accent':meta["original_data"]['accent'],
+				# 'language':t[0].split('/')[-3],
+				}
+
+		return audio, labels
+	
+	def to_keys(self, data):
+		audio, labels  = data
+
+		labels['age'] = (labels['age'], _AGE_DICT.get(labels['age'], None))
+		labels['gender'] = (labels['gender'], _GENDER_DICT.get(labels['gender'], None))
+
+		return audio, labels 
+	
+	def filter_fn(self, sample):
+		if _GENDER_DICT.get(sample[1]['gender'], None) and sample[1]['age']:
+			return True
+		return False
+
+	def create_pipeline(self, data_dir):
+		datapipe = torchdata.datapipes.iter.IterableWrapper(data_dir)\
+			.list_files_by_fsspec()\
+			.shuffle()\
+			.sharding_filter()\
+			.open_files_by_fsspec(mode='rb')\
+			.load_from_tar()\
+			.groupby(group_by_filename, group_size=2, guaranteed_group_size=2)\
+			.map(self.to_sampels)\
+			.filter(self.filter_fn)\
+			.map(self.to_keys)\
+			# .batch(self.batch_size) \
+			# .map(self.collate_fn)
+
+		return datapipe
+
+	def collate_fn(self, batch):
+		audios, labels = zip(*batch)
+
+		mels = []
+		for a, l in zip(audios, labels):
+			mel = librosa.feature.melspectrogram(y=a[0], sr=a[1], fmin=0, fmax=8000, n_mels=80, n_fft=1024, win_length=1024, hop_length=512)
+			mel = librosa.power_to_db(mel, ref=np.max)
+			mel = (mel+40)/40
+			mels.append(torch.tensor(mel, dtype=torch.float32).T)
+
+			l["text"] = torch.tensor(self.tokeniser.encode(", ".join(l["text"])))
+
+		mel_lengths = [mel.shape[0] for mel in mels]
+		mel_lengths = torch.tensor(mel_lengths)
+		text_lengths = [l["text"].size(0) for l in labels]
+		text_lengths = torch.tensor(text_lengths)
+
+		mels = pad_sequence(mels).permute(1,2,0).contiguous()
+
+		return labels, mels, text_lengths, mel_lengths
