4dd1cc067f97360f66764f2ea756e47b11d1a9ca
knoriy <knoriy@ip-26-0-130-164.ec2.internal>
2023-05-09 10:23:41 +0000

    added note for audioset setting

--- clasp/utils/preprocees_data.py
+++ clasp/utils/preprocees_data.py
@@ -18,6 +18,12 @@ class PreCacheTDM(MultilingualTDM):
 	def __init__(self, *args, **kwargs):
 		super().__init__(*args, **kwargs)
 
+		######## Uncomment for audioset ########
+		# self.train_data_dir = self.urls['balanced_train']
+		# self.test_data_dir = self.urls['unbalanced_train']
+		# self.valid_data_dir = self.urls['eval']
+		######## Uncomment for audioset ########
+
 	def to_sampels(self, data):
 		a, t = data
 		audio = soundfile.read(io.BytesIO(a[1].read()))
@@ -116,24 +122,26 @@ class PreCacheTDM(MultilingualTDM):
 def main():
 	batch_size = 64
 	samples_per_tar = 512
+	num_workers = 32
 
 	dataset = PreCacheTDM(
 			root_data_path='s3://s-laion-audio/webdataset_tar/', 
-			dataset_list='./config/dataset_list.txt',
+			dataset_list='./config/test_list.txt',
 			# exclude_list='./config/exclude_list.txt',
 			batch_size = batch_size,
-			num_workers=124,
+			num_workers=num_workers,
 			cache_path='./tmp/tensored_list.json', 
+			train_valid_test=['balanced_train', 'eval', 'unbalanced_train'],
 		)
 
 	dataset.setup()
 
-	# total = len(dataset.train_data_dir)*(samples_per_tar//batch_size)
-	# pbar = tqdm.tqdm(dataset.train_dataloader(), desc="train", total=total)
-	# for i in pbar:
-	# 	pbar.set_description(f"Processing {i}")
-	# 	pbar.update(1)
-	# print("train_dataloader end")
+	total = len(dataset.train_data_dir)*(samples_per_tar//batch_size)
+	pbar = tqdm.tqdm(dataset.train_dataloader(), desc="train", total=total)
+	for i in pbar:
+		pbar.set_description(f"Processing {i}")
+		pbar.update(1)
+	print("train_dataloader end")
 
 	# total = len(dataset.valid_data_dir)*(samples_per_tar//batch_size)
 	# pbar = tqdm.tqdm(dataset.val_dataloader(), desc="valid", total=total)
@@ -142,16 +150,17 @@ def main():
 	# 	pbar.update(1)
 	# print("val_dataloader end")
 
-	total = len(dataset.test_data_dir)*(samples_per_tar//batch_size)
-	pbar = tqdm.tqdm(dataset.test_dataloader(), desc="test", total=total)
-	for i in pbar:
-		pbar.set_description(f"Processing {i}")
-		pbar.update(1)
-	print("test_dataloader end")
+	# total = len(dataset.test_data_dir)*(samples_per_tar//batch_size)
+	# pbar = tqdm.tqdm(dataset.test_dataloader(), desc="test", total=total)
+	# for i in pbar:
+	# 	pbar.set_description(f"Processing {i}")
+	# 	pbar.update(1)
+	# print("test_dataloader end")
 
 	# dataset.teardown('fit')
 
 if __name__ == '__main__':
 	main()
 	upload_to_s3_cmd = "srun --partition=cpu64 --exclusive --comment clap --job-name=aws s3 cp --recursive ./tmp/tensored/ s3://s-laion/knoriy/tensored/ --dryrun"
+	# srun --partition=cpu32 --exclusive --comment clap --job-name=aws /fsx/home-knoriy/miniconda3/envs/clasp_2/bin/python clasp/utils/preprocees_data.py
 	print("end")
