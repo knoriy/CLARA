b398e91b7ac3c01da57236027a48bb6e6bbab4d3
knoriy <knoriy@ip-172-31-44-155.ec2.internal>
2023-05-23 17:32:57 +0000

    removed unwanted vars as they were causing PL slurm bugs

--- submit.sh
+++ submit.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #SBATCH --partition=g40
 #SBATCH --job-name=base_4
-#SBATCH --nodes=1
+#SBATCH --nodes=2
 #SBATCH --ntasks-per-node=8
 #SBATCH --gpus-per-node=8
 #SBATCH --cpus-per-task=12
@@ -14,19 +14,6 @@
 module load openmpi
 module load cuda/11.7
 
-export NCCL_PROTO=simple
-export FI_EFA_FORK_SAFE=1
-export FI_LOG_LEVEL=1
-export FI_EFA_USE_DEVICE_RDMA=1
-
-export PYTHONFAULTHANDLER=1
-
-export CUDA_LAUNCH_BLOCKING=1
-export OMPI_MCA_mtl_base_verbose=1
-export FI_EFA_ENABLE_SHM_TRANSFER=0
-export FI_PROVIDER=efa
-export FI_EFA_TX_MIN_CREDITS=64
-export NCCL_TREE_THRESHOLD=0
 
 srun /fsx/home-knoriy/miniconda3/envs/clasp_2/bin/python /fsx/knoriy/code/CLASP/clasp/train.py fit\
     --config ./config/config/base.yaml \
@@ -35,5 +22,5 @@ srun /fsx/home-knoriy/miniconda3/envs/clasp_2/bin/python /fsx/knoriy/code/CLASP/
     --model ./config/config/model/pl_clasp.yaml \
     --data ./config/config/data/tensored.yaml \
     --trainer.num_nodes $SLURM_JOB_NUM_NODES \
-    --data.num_workers 12 \
+    --data.num_workers 16 \
     --trainer.logger.name AudioSet_90m_T-Pio \
