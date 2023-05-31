a9ae7d04ca3b4a320b1cfc17f9453445cdddd3b1
knoriy <knoriy@ip-172-31-44-155.ec2.internal>
2023-05-31 13:56:27 +0000

    updated to include dataset name

--- clasp/datamodule/utils.py
+++ clasp/datamodule/utils.py
@@ -12,7 +12,8 @@ def get_log_melspec(audio, sr, n_mels=80, n_fft=1024, hop_length=512, win_length
 
 def filepath_fn(url):
 	temp_dir = tempfile.gettempdir()
-	return os.path.join(temp_dir, "CLASP", os.path.basename(url))
+	_, dataset_name, folder_name, file_name = url.rsplit('/', 3)
+	return os.path.join(temp_dir, "CLASP", dataset_name, folder_name, file_name)
 
 def group_by_filename(x):
 	return os.path.basename(x[0]).split(".")[0]
