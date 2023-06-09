0deb4f85ff66afd953ce6ac3d77549ece85abcc1
knoriy <knoriy@ip-172-31-44-155.ec2.internal>
2023-06-09 20:14:10 +0000

    added util fn for torchdata

--- clasp/datamodule/utils.py
+++ clasp/datamodule/utils.py
@@ -3,6 +3,7 @@ import tempfile
 import librosa
 import numpy as np
 import torch
+import glob
 
 def get_log_melspec(audio, sr, n_mels=80, n_fft=1024, hop_length=512, win_length=1024, fmin=0, fmax=8000, **kwargs):
     mel = librosa.feature.melspectrogram(y=audio, sr=sr, n_mels=n_mels, n_fft=n_fft, win_length=win_length, hop_length=hop_length, fmin=fmin, fmax=fmax, **kwargs)
@@ -14,6 +15,19 @@ def filepath_fn(url):
 	temp_dir = tempfile.gettempdir()
 	_, dataset_name, folder_name, file_name = url.rsplit('/', 3)
 	return os.path.join(temp_dir, "CLASP", dataset_name, folder_name, file_name)
+
+def delete_primise_fn(url):
+	files = glob.glob(f"{url}.*")
+	for file in files:
+		os.remove(file)
+	return url
+
+def delete_cache_fn(cache_path):
+    tar_path = os.path.dirname(cache_path[0])
+    if os.path.isfile(tar_path):
+        os.remove(tar_path)
+    return cache_path
+
 
 def group_by_filename(x):
 	return os.path.basename(x[0]).split(".")[0]
