8c046910c7ce4962c4009dbb85e9c0582811cc37
knoriy <knoriy@ip-26-0-146-37.us-west-2.compute.internal>
2023-06-19 14:26:30 +0000

    updated cluster setup

--- submit.sh
+++ submit.sh
@@ -1,19 +1,20 @@
 #!/bin/bash
-#SBATCH --partition=g40
-#SBATCH --job-name=base_4
+#SBATCH --partition=g40x
+#SBATCH --job-name=clsp
 #SBATCH --nodes=1
 #SBATCH --ntasks-per-node=1
 #SBATCH --gpus-per-node=1
 #SBATCH --cpus-per-task=12
-#SBATCH --account clsp
+#SBATCH --account laion
 #SBATCH --output=logs/outs/%x_%j.out
 #SBATCH --signal=SIGUSR1@90
 #SBATCH --signal=SIGTERM@90
-#SBATCH --exclude ip-26-0-134-43,ip-26-0-131-108,ip-26-0-140-150,ip-26-0-143-39
 
 module load openmpi
 
-srun /fsx/home-knoriy/miniconda3/envs/clasp_2/bin/python /fsx/knoriy/code/CLASP/clasp/train.py fit\
+/admin/home-knoriy/miniconda3/envs/clasp/bin/python /fsx/knoriy/CLASP/scripts/download_from_s3.py
+
+srun /admin/home-knoriy/miniconda3/envs/clasp/bin/python /fsx/knoriy/CLASP/clasp/train.py fit\
     --config ./config/config/base.yaml \
     --trainer ./config/config/trainer/base.yaml \
     --trainer ./config/config/trainer/slurm.yaml \
@@ -23,4 +24,6 @@ srun /fsx/home-knoriy/miniconda3/envs/clasp_2/bin/python /fsx/knoriy/code/CLASP/
     --data.num_workers 12 \
     --data.batch_size 16 \
     --trainer.logger.name large \
-    --trainer.max_epochs 200 \
+    --trainer.max_epochs 1 \
+
+bash scripts/clean_tmp_files.sh
