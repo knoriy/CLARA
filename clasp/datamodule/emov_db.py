15c2503eac1abea6decd4e6c93ad11df0bf5e639
knoriy <knoriy@ip-26-0-145-145.us-west-2.compute.internal>
2023-07-03 12:23:39 +0000

    emov-db setup

--- clasp/datamodule/emov_db.py
+++ clasp/datamodule/emov_db.py
@@ -1,25 +1,82 @@
+from typing import Optional
+
 import io
 import json
 import soundfile
 
 import torch
 import torchdata
+from torchdata.datapipes.iter import FSSpecFileOpener
 from torch.nn.utils.rnn import pad_sequence
 
 # from .base_tdm import BaseTDM
 from datamodule.base_tdm import BaseTDM
 from text.tokeniser import Tokeniser # from text.whisper.tokenizer import get_tokenizer
 
+from utils import get_s3_paths, get_local_paths, get_lists 
 from .utils import get_log_melspec, group_by_filename
+from .utils import Boto3FileOpenerIterDataPipe as Boto3FileOpener
 
+import logging
+pl_logger = logging.getLogger('pytorch_lightning')
 
 _EMOTION_DICT = {"angry": 0,"disgusted": 1,"amused": 2,"sleepy": 3,"neutral": 4}
 _GENDER_DICT = {"male": 0,"male": 0, "female": 1, "females": 1}
 
 class EmovDBTDM(BaseTDM):
-	def __init__(self, *args, **kwargs):
+	def __init__(self,
+			root_data_path:str,
+			exclude_list:Optional[str]=None,
+			cache_path:Optional[str]=None,
+			use_cache:Optional[bool]=True,
+			recache:Optional[bool]=False,
+			train_valid_test:Optional[list]=['train', 'valid', 'test'],
+			*args, **kwargs):
 		super().__init__(*args, **kwargs)
 		self.tokeniser = Tokeniser()
+
+		exclude = []
+		if exclude_list:
+			exclude = get_lists(exclude_list)
+		
+		dataset_name = 'EmoV_DB'
+
+		if not cache_path:
+			cache_path = f"logs/{dataset_name}.json"
+
+		if root_data_path.startswith('s3://'):
+			self.is_local = False
+			root_data_path = root_data_path.replace('s3://', '')
+			self.urls = get_s3_paths(
+				base_path			= root_data_path,
+				train_valid_test	= train_valid_test,
+				dataset_names		= dataset_name, 
+				exclude				= exclude,
+				cache_path			= cache_path,
+				use_cache			= use_cache,
+				recache				= recache
+				)
+		else:
+			self.is_local = True
+			self.urls = get_local_paths(
+				base_path			= root_data_path,
+				train_valid_test	= train_valid_test,
+				dataset_names		= dataset_name, 
+				exclude				= exclude,
+				cache_path			= cache_path,
+				use_cache			= use_cache,
+				recache				= recache
+				)
+
+		self.train_data_dir = self.urls.get('train', None)
+		self.test_data_dir = self.urls.get('test', None)
+		self.valid_data_dir = self.urls.get('valid', None)
+
+		pl_logger.info(f"Urls found: \
+			\n\tTrain: {len(self.urls.get('train', None))} \
+			\n\tValid: {len(self.urls.get('valid', None))} \
+			\n\tTest: {len(self.urls.get('test', None))}"
+		)
 
 	def to_sampels(self, data):
 		a, t = data
@@ -43,17 +100,24 @@ class EmovDBTDM(BaseTDM):
 
 	def create_pipeline(self, data_dir):
 		datapipe = torchdata.datapipes.iter.IterableWrapper(data_dir)\
-			.list_files_by_fsspec(masks=["*.tar"])\
-			.filter(self.exclude_fn)\
-			.sharding_filter()\
-			.open_files_by_fsspec(mode='rb')\
-			.load_from_tar()\
-			.groupby(group_by_filename, group_size=2, guaranteed_group_size=2)\
-			.shuffle(buffer_size=1000)\
-			.map(self.to_sampels)\
+			.shuffle()\
+			.sharding_filter()
+			
+		if self.is_local:
+			datapipe = FSSpecFileOpener(datapipe, mode='rb')
+		else:
+			datapipe = Boto3FileOpener(datapipe, mode='rb')
+
+		datapipe = datapipe.load_from_tar() \
+			.batch(2) \
+			.shuffle(buffer_size=100)\
+			.map(self.to_sampels) \
 			.map(self.to_keys)\
-			# .batch(self.batch_size) \
-			# .map(self.collate_fn)
+
+		if self.dataloader2:
+			datapipe = datapipe.batch(self.batch_size) \
+				.map(self.collate_fn)\
+				.fullsync()
 
 		return datapipe
 
