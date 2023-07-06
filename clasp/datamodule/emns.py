2ddf2f758eb8c5a93e1620274a7e1e8b7bf36cd3
knoriy <knoriy@ip-26-0-151-58.us-west-2.compute.internal>
2023-07-06 09:25:05 +0000

    updated emns to use processed file on s3 and local

--- clasp/datamodule/emns.py
+++ clasp/datamodule/emns.py
@@ -1,59 +1,147 @@
-import os
+from typing import Optional
+
+import io
 import json
 import soundfile
+
 import torch
 import torchdata
+from torchdata.datapipes.iter import FSSpecFileOpener
 from torch.nn.utils.rnn import pad_sequence
 
 # from .base_tdm import BaseTDM
-from .base_tdm import BaseTDM
-from .utils import get_log_melspec
+from datamodule.base_tdm import BaseTDM
+from text.tokeniser import Tokeniser # from text.whisper.tokenizer import get_tokenizer
 
-KEYS = ("id","utterance","description","emotion","date_created","status","gender","age","level","audio_recording","user_id")
+from utils import get_s3_paths, get_local_paths, get_lists 
+from .utils import get_log_melspec, group_by_filename
+from .utils import Boto3FileOpenerIterDataPipe as Boto3FileOpener
 
-def parse_csv(data):
-	data = data[1].split('|')
-	data = {k:v for k,v in zip(KEYS, data)}
-	return data
+import logging
+pl_logger = logging.getLogger('pytorch_lightning')
+
+_EMOTION_DICT = {"angry": 0,"disgusted": 1,"fearful": 2,"happy": 3,"neutral": 4, "sad": 5}
+_GENDER_DICT = {"male": 0,"males": 0, "Male": 0, "female": 1, "females": 1, "Female": 1}
 
 class EMNSTDM(BaseTDM):
-	def __init__(self, classes:str, *args, **kwargs):
+	def __init__(self,
+			root_data_path:str,
+			exclude_list:Optional[str]=None,
+			cache_path:Optional[str]=None,
+			use_cache:Optional[bool]=True,
+			recache:Optional[bool]=False,
+			train_valid_test:Optional[list]=['train', 'valid', 'test'],
+			*args, **kwargs):
 		super().__init__(*args, **kwargs)
-		with open(classes, 'r') as f:
-			self.emotions = json.load(f)
-	def to_sampels(self, data):
-		path, label = data['audio_recording'], data['emotion']
-		path = os.path.basename(path).replace('.webm', '.flac')
-		path = os.path.join('/fsx/knoriy/raw_datasets/EMNS/cleaned_webm', path)
+		self.tokeniser = Tokeniser()
 
-		return soundfile.read(path), label.strip().lower()
+		exclude = []
+		if exclude_list:
+			exclude = get_lists(exclude_list)
+		
+		dataset_name = 'EMNS'
+
+		if not cache_path:
+			cache_path = f"logs/{dataset_name}.json"
+
+		if root_data_path.startswith('s3://'):
+			self.is_local = False
+			root_data_path = root_data_path.replace('s3://', '')
+			self.urls = get_s3_paths(
+				base_path			= root_data_path,
+				train_valid_test	= train_valid_test,
+				dataset_names		= dataset_name, 
+				exclude				= exclude,
+				cache_path			= cache_path,
+				use_cache			= use_cache,
+				recache				= recache
+				)
+		else:
+			self.is_local = True
+			self.urls = get_local_paths(
+				base_path			= root_data_path,
+				train_valid_test	= train_valid_test,
+				dataset_names		= dataset_name, 
+				exclude				= exclude,
+				cache_path			= cache_path,
+				use_cache			= use_cache,
+				recache				= recache
+				)
+
+		self.train_data_dir = self.urls.get('train', None)
+		self.test_data_dir = self.urls.get('test', None)
+		self.valid_data_dir = self.urls.get('valid', None)
+
+		pl_logger.info(f"Urls found: \
+			\n\tTrain: {len(self.urls.get('train', None))} \
+			\n\tValid: {len(self.urls.get('valid', None))} \
+			\n\tTest: {len(self.urls.get('test', None))}"
+		)
+
+	def to_sampels(self, data):
+		a, t = data
+		return soundfile.read(io.BytesIO(a[1].read())), json.loads(t[1].read().decode('utf-8'))
+	
+	def to_keys(self, data):
+		audio, labels  = data
+
+		new_labels = {}
+
+		new_labels['text'] = labels['text']
+		new_labels['emotion'] = _EMOTION_DICT.get(labels['original_data']['emotion'].lower())
+		new_labels['gender'] = _GENDER_DICT.get(labels['original_data']['gender'])
+
+		return audio, new_labels 
+	
+	def filter_fn(self, sample):
+		if _GENDER_DICT.get(sample[1]['gender'], None) and sample[1]['age']:
+			return True
+		return False
 
 	def create_pipeline(self, data_dir):
 		datapipe = torchdata.datapipes.iter.IterableWrapper(data_dir)\
-			.open_files()\
-			.sharding_filter()\
 			.shuffle()\
-			.readlines(skip_lines=1)\
-			.map(parse_csv)\
-			.map(self.to_sampels)\
-			.shuffle()\
-			# .batch(self.batch_size) \
-			# .map(self.collate_fn)
+			.sharding_filter()
+			
+		if self.is_local:
+			datapipe = FSSpecFileOpener(datapipe, mode='rb')
+		else:
+			datapipe = Boto3FileOpener(datapipe, mode='rb')
+
+		datapipe = datapipe.load_from_tar() \
+			.batch(2) \
+			.shuffle(buffer_size=100)\
+			.map(self.to_sampels) \
+			# .map(self.to_keys)\
+
+		if self.dataloader2:
+			datapipe = datapipe.batch(self.batch_size) \
+				.map(self.collate_fn)\
+				.fullsync()
 
 		return datapipe
 
 	def collate_fn(self, batch):
 		audios, labels = zip(*batch)
 
-		labels = torch.tensor([self.emotions[l] for l in labels])
+		texts = [torch.tensor(self.tokeniser.encode(", ".join(l["text"]))) for l in labels]
+		genders = torch.tensor([label['gender'] for label in labels])
+		emotion = torch.tensor([label['emotion'] for label in labels])
 
 		mels = [get_log_melspec(a[0], a[1]) for a in audios]
-
 		mel_lengths = [mel.shape[0] for mel in mels]
 		mel_lengths = torch.tensor(mel_lengths)
-		text_lengths = [1 for _ in labels]
+		
+		text_lengths = [text.size(0) for text in texts]
 		text_lengths = torch.tensor(text_lengths)
 
 		mels = pad_sequence(mels).permute(1,2,0).contiguous()
+		texts = pad_sequence(texts).T.contiguous()
 
-		return labels, mels, text_lengths, mel_lengths
+		new_labels = {
+			"texts": texts,
+			"gender": genders,
+			"emotion": emotion,
+			}
+
+		return new_labels, mels, text_lengths, mel_lengths
