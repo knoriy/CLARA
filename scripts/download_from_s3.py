5abda7063c79d6aa4b18a42e8dab734dfe2bdc19
knoriy <knoriy72@gmail.com>
2023-07-27 11:02:24 +0000

    added option to use aws cli backend to download from s3

--- scripts/download_from_s3.py
+++ scripts/download_from_s3.py
@@ -3,6 +3,9 @@ import sys
 sys.path.append("/fsx/knoriy/CLASP/clasp/")
 from typing import Any, Callable, Deque, Dict, Iterator, List, Optional, Tuple, TypeVar
 import yaml
+import subprocess
+import tempfile
+
 
 import torchdata
 from torchdata.datapipes import functional_datapipe
@@ -69,6 +72,13 @@ class MOnDiskCacheHolderIterDataPipe(OnDiskCacheHolderIterDataPipe):
 
         return int(result)
 
+def download_s3_folder(root_s3_path, dataset_name, save_dir):
+    command = f"aws s3 cp {os.path.join(root_s3_path, dataset_name, '')} {save_dir} --recursive"
+    process = subprocess.Popen(command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    _, stderr = process.communicate()
+    if process.returncode != 0:
+        print(f"Error downloading folder from S3: {stderr.decode()}")
+
 def main(args):
     with open('/fsx/knoriy/CLASP/config/config/data/base.yaml') as file:
         config = yaml.full_load(file)['init_args']
@@ -76,37 +86,46 @@ def main(args):
     exclude_names = get_lists(config['exclude_list'])
     root_data_path = 's3://laion-west-audio/webdataset_tar/'
 
+    if args.backend == 'awscli':
+        temp_dir = tempfile.gettempdir()
+        for dataset_name in tqdm.tqdm(dataset_names, desc='Downloading: '):
+            save_dir = os.path.join(temp_dir, "CLASP", 'laion-west-audio/webdataset_tar/', dataset_name)
+            download_s3_folder(root_s3_path=root_data_path, dataset_name=dataset_name, save_dir=save_dir)
 
-    root_data_path = root_data_path.replace('s3://', '')
-    urls = get_s3_paths(
-        base_path			= root_data_path,
-        train_valid_test	= config['train_valid_test'],
-        dataset_names		= dataset_names, 
-        exclude				= exclude_names,
-        recache				= True
-        )
-    
-    updated_urls = []
-    for key in urls.keys():
-        updated_urls.extend(urls[key])
-    
-    datapipe = torchdata.datapipes.iter.IterableWrapper(updated_urls)\
-        .sharding_filter()\
-
-    datapipe = MOnDiskCacheHolderIterDataPipe(datapipe, filepath_fn=filepath_fn)\
-        .open_files_by_boto3(mode='rb')\
-        .end_caching(filepath_fn=filepath_fn)\
+    elif args.backend == 'tdm':
+        root_data_path = root_data_path.replace('s3://', '')
+        urls = get_s3_paths(
+            base_path			= root_data_path,
+            train_valid_test	= config['train_valid_test'],
+            dataset_names		= dataset_names, 
+            exclude				= exclude_names,
+            recache				= True
+            )
         
-    dl = DataLoader(datapipe)
-    
-    for _ in tqdm.tqdm(dl, total=len(updated_urls), desc='Downloading'):
-        pass
+        updated_urls = []
+        for key in urls.keys():
+            updated_urls.extend(urls[key])
+        
+        datapipe = torchdata.datapipes.iter.IterableWrapper(updated_urls)\
+            .sharding_filter()\
+
+        datapipe = MOnDiskCacheHolderIterDataPipe(datapipe, filepath_fn=filepath_fn)\
+            .open_files_by_boto3(mode='rb')\
+            .end_caching(filepath_fn=filepath_fn)\
+            
+        dl = DataLoader(datapipe)
+        
+        for _ in tqdm.tqdm(dl, total=len(updated_urls), desc='Downloading'):
+            pass
 
 if __name__ == '__main__':
     import argparse
     parser = argparse.ArgumentParser()
 
     parser.add_argument('--data', type=str, help='Path to data config file')
+    parser.add_argument('--backend', type=str, choices=['tdm', 'awscli'], help='what backend to use, aws cli with download files using the cli or tdm will use torchdata to download files')
 
     args = parser.parse_args()
     main(args)
+
+    # python scripts/download_from_s3.py --data ./config/config/data/base.yaml --backend awscli
