083cc0de2bad7c43b09965555d90dc8995153964
knoriy <knoriy72@gmail.com>
2023-07-27 18:22:25 +0000

    upated cmd

--- submit.sh
+++ submit.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #SBATCH --partition=g40x
-#SBATCH --job-name=clsp60M
+#SBATCH --job-name=clsp100M
 #SBATCH --nodes=1
 #SBATCH --ntasks-per-node=8
 #SBATCH --gpus-per-node=8
@@ -11,17 +11,17 @@
 #SBATCH --signal=SIGTERM@90
 
 module load openmpi
-hostname # for debugging, logs the hostname to the output file
+srun hostname # for debugging, logs the hostname to the output file
 
-LOGGER_NAME=balanced_audioset
+LOGGER_NAME=CLASP_100M
 
-MODEL_CONF_PATH=./config/config/model/pl_clasp_60M.yaml
+MODEL_CONF_PATH=./config/config/model/pl_clasp_100M.yaml
 BASE_CONF_PATH=./config/config/base.yaml
 TRAINER_BASE_CONF_PATH=./config/config/trainer/base.yaml
 TRAINER_CONF_PATH=./config/config/trainer/slurm.yaml
 DATA_CONF_PATH=./config/config/data/base.yaml
 
-/admin/home-knoriy/miniconda3/envs/clasp/bin/python /fsx/knoriy/CLASP/scripts/download_from_s3.py --data $DATA_CONF_PATH --backend awscli
+srun /admin/home-knoriy/miniconda3/envs/clasp/bin/python /fsx/knoriy/CLASP/scripts/download_from_s3.py --data $DATA_CONF_PATH --backend awscli
 
 srun /admin/home-knoriy/miniconda3/envs/clasp/bin/python /fsx/knoriy/CLASP/clasp/train.py fit\
     --config $BASE_CONF_PATH \
@@ -31,8 +31,8 @@ srun /admin/home-knoriy/miniconda3/envs/clasp/bin/python /fsx/knoriy/CLASP/clasp
     --data $DATA_CONF_PATH \
     --trainer.num_nodes $SLURM_JOB_NUM_NODES \
     --data.num_workers 6 \
-    --data.batch_size 816 \
+    --data.batch_size 16 \
     --trainer.logger.name $LOGGER_NAME \
     --trainer.max_epochs 120 \
 
-# sh /fsx/knoriy/CLASP/scripts/clean_tmp_files.sh
+# srun sh /fsx/knoriy/CLASP/scripts/clean_tmp_files.sh
