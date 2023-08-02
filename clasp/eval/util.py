3f42b00598347ed0fcd41887f385d09a05ac62df
knoriy <knoriy72@gmail.com>
2023-08-02 13:40:56 +0000

    root data path is now a controlable variable

--- clasp/eval/util.py
+++ clasp/eval/util.py
@@ -7,7 +7,7 @@ import json
 from datamodule import *
 from utils import get_lists
 
-def get_dataset(task, dataset_name, root_cfg_path, batch_size=1, num_workers=0):
+def get_dataset(task, dataset_name, root_cfg_path, root_data_path='s3://laion-west-audio/webdataset_tar/', batch_size=1, num_workers=0):
     ##########
 	# Sounds
 	##########
@@ -18,7 +18,7 @@ def get_dataset(task, dataset_name, root_cfg_path, batch_size=1, num_workers=0):
 			with open(os.path.join(root_cfg_path , "classification/sounds/esc-50/minor_classes.json")) as f:
 				classes = json.load(f)
 			dataset = ESC50TDM(
-						root_data_path='s3://laion-west-audio/webdataset_tar/',
+						root_data_path=root_data_path,
 						classes=classes,
 						batch_size = batch_size,
 						num_workers = num_workers,
@@ -28,7 +28,7 @@ def get_dataset(task, dataset_name, root_cfg_path, batch_size=1, num_workers=0):
 			with open(os.path.join(root_cfg_path , "classification/sounds/audioset/classes.json")) as f:
 				classes = json.load(f)
 			dataset = AudioSetTDM(
-						root_data_path='s3://laion-west-audio/webdataset_tar/',
+						root_data_path=root_data_path,
 						classes=classes,
 						batch_size = batch_size,
 						num_workers = num_workers,
@@ -38,7 +38,7 @@ def get_dataset(task, dataset_name, root_cfg_path, batch_size=1, num_workers=0):
 			with open(os.path.join(root_cfg_path , "classification/sounds/us8k/classes.json")) as f:
 				classes = json.load(f)
 			dataset = Urbansound8KTDM(
-						root_data_path='s3://laion-west-audio/webdataset_tar/',
+						root_data_path=root_data_path,
 						batch_size = batch_size,
 						num_workers = num_workers,
 					)
@@ -47,7 +47,7 @@ def get_dataset(task, dataset_name, root_cfg_path, batch_size=1, num_workers=0):
 			with open(os.path.join(root_cfg_path , "classification/sounds/fsd50k/classes.json")) as f:
 				classes = json.load(f)
 			dataset = FSD50KTDM(
-						root_data_path='s3://laion-west-audio/webdataset_tar/',
+						root_data_path=root_data_path,
 						classes=classes,
 						batch_size = batch_size,
 						num_workers = num_workers,
@@ -75,7 +75,7 @@ def get_dataset(task, dataset_name, root_cfg_path, batch_size=1, num_workers=0):
 
 		if dataset_name == 'emns':
 			dataset = EMNSTDM(
-						root_data_path='s3://laion-west-audio/webdataset_tar/',
+						root_data_path=root_data_path,
 						batch_size = batch_size,
 						num_workers = num_workers,
 					)
@@ -85,7 +85,7 @@ def get_dataset(task, dataset_name, root_cfg_path, batch_size=1, num_workers=0):
 				classes = json.load(f)
 		elif dataset_name == 'emov-db':
 			dataset = EmovDBTDM(
-				root_data_path='s3://laion-west-audio/webdataset_tar/',
+				root_data_path=root_data_path,
 				batch_size = batch_size,
 				num_workers = num_workers,
 			)
@@ -96,7 +96,7 @@ def get_dataset(task, dataset_name, root_cfg_path, batch_size=1, num_workers=0):
 		elif dataset_name == 'crema-d':
 			Warning("CREMA-D is not supported yet")
 			dataset = CremaDTDM(
-				root_data_path='s3://laion-west-audio/webdataset_tar/',
+				root_data_path=root_data_path,
 				batch_size = batch_size,
 				num_workers = num_workers,
 			)
@@ -106,7 +106,7 @@ def get_dataset(task, dataset_name, root_cfg_path, batch_size=1, num_workers=0):
 				classes = json.load(f)
 		elif dataset_name == 'ravdess':
 			dataset = RavdessTDM(
-						root_data_path='s3://laion-west-audio/webdataset_tar/',
+						root_data_path=root_data_path,
 						batch_size = batch_size,
 						num_workers = num_workers,
 					)
